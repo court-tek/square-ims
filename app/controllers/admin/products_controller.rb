a909d5c227512904b2fca6ba486e95f127657154
court-tek <dishoughng@gmail.com>
2023-12-20 12:36:55 -0500

    setting up products api controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,26 @@
 class Admin::ProductsController < ApplicationController
+    require 'square'
     layout 'admin'
 
     def index
     end
 
     def new
+    end
+
+    private
+    def get_square_client
+        access_token = ENV['SQUARE_ACCESS_TOKEN']
+    end
+
+    def retrieve_catalog_objects
+        client = self.get_square_client
+        result = client.catalog.list_catalog
+
+        if result.success?
+        puts result.data
+        elsif result.error?
+        warn result.errors
+        end
     end
 end
