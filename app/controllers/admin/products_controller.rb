c34d81759cbef8a73e9c7e8e8417cc4a64f5fc0e
court-tek <dishoughng@gmail.com>
2023-12-21 05:53:26 -0500

    finally iterated over the json data from the api finally

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,30 +1,38 @@
 class Admin::ProductsController < ApplicationController
     require 'square'
+    require 'json'
     layout 'admin'
 
+    # GET all catalog objects from api
     def index
+        @catalog = retrieve_catalog_objects
     end
 
+    # 
     def new
     end
 
     private
-    def get_square_client
-        access_token = ENV.fetch('SQUARE_ACCESS_TOKEN')
-        client = Square::Client.new(
-            access_token: access_token,
-            environment: 'sandbox'
-        )
-    end
+        # connects to the square client api
+        def get_square_client
+            access_token = ENV.fetch('SQUARE_ACCESS_TOKEN')
+            client = Square::Client.new(
+                access_token: access_token,
+                environment: 'sandbox'
+            )
 
-    def retrieve_catalog_objects
-        client = self.get_square_client
-        result = client.catalog.list_catalog
-
-        if result.success?
-        puts result.data
-        elsif result.error?
-        warn result.errors
+            return client
         end
-    end
+
+        # retrieve all catalog objects with no preference
+        def retrieve_catalog_objects
+            client = self.get_square_client
+            result = client.catalog.list_catalog
+
+            if result.success?
+                return result.data.objects
+            elsif result.error?
+                warn result.errors
+            end
+        end
 end
